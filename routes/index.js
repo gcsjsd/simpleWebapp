67cc56afde1df864c16ac514c4286357d063b1b9
Ge Chang <chg073@eng.ucsd.edu>
2017-11-26 00:07:49 -0800

    the simple website

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/helloworld',function(req,res){
+  res.render('helloworld',{title:'Hello World!'});
+});
+
+router.get('/userlist',function(req,res){
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist',{
+        "userlist":docs
+    });
+  });
+});
+
+router.get('/newuser',function(req,res){
+  res.render('newuser',{title:"Add new User"});
+});
+
+module.exports = router;
